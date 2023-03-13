a77df6c263d0934f6d6ae116a53ebe523ef7dd95
colina-x <71702031+colina-x@users.noreply.github.com>
2023-03-13 06:59:03 +0000

    test0.1

--- dungeon.js
+++ dungeon.js
@@ -1,5 +1,23 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(0, 0, 150, 100);
+const SIZE = 10
+const WIDTH = 200
+const HEIGHT = 150
+const COLOR=["green","red","pink"]
+class rect{
+    constructor(x,y,width,height) {
+        this.x = x
+        this.y = y
+        this.width = width * SIZE
+        this.height = height * SIZE
+    }
+}
+ind = 0
+ctx.fillStyle = COLOR[ind];
+for (i in Range(WIDTH / SIZE)) {
+    for (j in Range(HEIGHT / SIZE)) {
+        ctx.fillRect(i, j, SIZE, SIZE)
+        ctx.fillStyle = COLOR[++ind];
+    }
+}
