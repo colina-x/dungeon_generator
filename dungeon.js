9b880076e68b4699901139a3f74dd7ff8ac5d087
colina-x <zx1601288258@outlook.com>
2023-03-30 16:04:24 +0800

    3.30

--- dungeon.js
+++ dungeon.js
@@ -5,27 +5,317 @@ const SIZE = 5
 const WIDTH = 395
 const HEIGHT = 295
 const COLOR=["green","red","pink","blue","yellow","skyblue","#00ff00","#ddd","#733"]
-class Room{
-    constructor(x,y,width,height) {
-        this.x = x
-        this.y = y
+var Rect = function(x,y,width,height) {
+    this.x = x || 0
+    this.y = y || 0
+    this.width = width || 0
+    this.height = height || 0
+}
+Rect.prototype = {
+    reset: function ( x, y,width,height ) {
+
+		this.x = x;
+		this.y = y;
         this.width = width
         this.height = height
+		return this;
+
+	},
+
+	clone : function () {
+		return new Rect(this.x, this.y, this.width, this.height);
+	},
+
+    copyTo : function (r) {
+		r.x = this.x;
+		r.y = this.y;
+        r.width = this.width;
+		r.height = this.height;
+	},
+    getTop : function(){
+        return this.y;
+    },
+    getBottom : function(){
+        return this.y + this.height;
+    },
+    getLeft : function(){
+        return this.x;
+    },
+    getRight : function(){
+        return this.x + this.width;
+    },
+	points : function(){
+        var temp = []
+        for(let i=this.x;i<this.getRight();i++) {
+            for (let j = this.y; j < this.getBottom(); j++) {
+                temp.push(new Vector2(i,j));                
+            }
+        }
+        return temp;
+    },
+    inflate : function(n){
+        this.x+=n;
+        this.y+=n;
+        this.width-=n;
+        this.height-=n;
+        return this;
+    },
+    value : function() {
+        return this;
     }
+
 }
-class Vector2D{
-    constructor(x,y){
-        this.x=x
-        this.y=y
-    }
+
+var Vector2 = function (x,y) {
+	
+	this.x= x || 0; 
+	this.y = y || 0; 
+	
+};
+
+
+
+Vector2.prototype = {
+
+	reset: function ( x, y ) {
+
+		this.x = x;
+		this.y = y;
+
+		return this;
+
+	},
+    value : function(){
+        return [this.x,this.y]
+    },
+    add : function(dir){
+        return new Vector2(this.x+dir[0],this.y+dir[1]);
+    },
+	toString : function (decPlaces) {
+	 	decPlaces = decPlaces || 3; 
+		var scalar = Math.pow(10,decPlaces); 
+		return "[" + Math.round (this.x * scalar) / scalar + ", " + Math.round (this.y * scalar) / scalar + "]";
+	},
+	
+	clone : function () {
+		return new Vector2(this.x, this.y);
+	},
+	
+	copyTo : function (v) {
+		v.x = this.x;
+		v.y = this.y;
+	},
+	
+	copyFrom : function (v) {
+		this.x = v.x;
+		this.y = v.y;
+	},	
+	
+	magnitude : function () {
+		return Math.sqrt((this.x*this.x)+(this.y*this.y));
+	},
+	
+	magnitudeSquared : function () {
+		return (this.x*this.x)+(this.y*this.y);
+	},
+	
+	normalise : function () {
+		
+		var m = this.magnitude();
+				
+		this.x = this.x/m;
+		this.y = this.y/m;
+
+		return this;	
+	},
+	
+	reverse : function () {
+		this.x = -this.x;
+		this.y = -this.y;
+		
+		return this; 
+	},
+	
+	plusEq : function (v) {
+		this.x+=v.x;
+		this.y+=v.y;
+		
+		return this; 
+	},
+	
+	plusNew : function (v) {
+		 return new Vector2(this.x+v.x, this.y+v.y); 
+	},
+	
+	minusEq : function (v) {
+		this.x-=v.x;
+		this.y-=v.y;
+		
+		return this; 
+	},
+
+	minusNew : function (v) {
+	 	return new Vector2(this.x-v.x, this.y-v.y); 
+	},	
+	
+	multiplyEq : function (scalar) {
+		this.x*=scalar;
+		this.y*=scalar;
+		
+		return this; 
+	},
+	
+	multiplyNew : function (scalar) {
+		var returnvec = this.clone();
+		return returnvec.multiplyEq(scalar);
+	},
+	
+	divideEq : function (scalar) {
+		this.x/=scalar;
+		this.y/=scalar;
+		return this; 
+	},
+	
+	divideNew : function (scalar) {
+		var returnvec = this.clone();
+		return returnvec.divideEq(scalar);
+	},
+
+	dot : function (v) {
+		return (this.x * v.x) + (this.y * v.y) ;
+	},
+	
+	angle : function (useRadians) {
+		
+		return Math.atan2(this.y,this.x) * (useRadians ? 1 : Vector2Const.TO_DEGREES);
+		
+	},
+	
+	rotate : function (angle, useRadians) {
+		
+		var cosRY = Math.cos(angle * (useRadians ? 1 : Vector2Const.TO_RADIANS));
+		var sinRY = Math.sin(angle * (useRadians ? 1 : Vector2Const.TO_RADIANS));
+	
+		Vector2Const.temp.copyFrom(this); 
+
+		this.x= (Vector2Const.temp.x*cosRY)-(Vector2Const.temp.y*sinRY);
+		this.y= (Vector2Const.temp.x*sinRY)+(Vector2Const.temp.y*cosRY);
+		
+		return this; 
+	},	
+		
+	equals : function (v) {
+		return((this.x==v.x)&&(this.y==v.x));
+	},
+	
+	isCloseTo : function (v, tolerance) {	
+		if(this.equals(v)) return true;
+		
+		Vector2Const.temp.copyFrom(this); 
+		Vector2Const.temp.minusEq(v); 
+		
+		return(Vector2Const.temp.magnitudeSquared() < tolerance*tolerance);
+	},
+	
+	rotateAroundPoint : function (point, angle, useRadians) {
+		Vector2Const.temp.copyFrom(this); 
+		//trace("rotate around point "+t+" "+point+" " +angle);
+		Vector2Const.temp.minusEq(point);
+		//trace("after subtract "+t);
+		Vector2Const.temp.rotate(angle, useRadians);
+		//trace("after rotate "+t);
+		Vector2Const.temp.plusEq(point);
+		//trace("after add "+t);
+		this.copyFrom(Vector2Const.temp);
+		
+	}, 
+	
+	isMagLessThan : function (distance) {
+		return(this.magnitudeSquared()<distance*distance);
+	},
+	
+	isMagGreaterThan : function (distance) {
+		return(this.magnitudeSquared()>distance*distance);
+	}
+	
+	
+	// still AS3 to convert : 
+	// public function projectOnto(v:Vector2) : Vector2
+	// {
+	// 		var dp:Number = dot(v);
+	// 
+	// 		var f:Number = dp / ( v.x*v.x + v.y*v.y );
+	// 
+	// 		return new Vector2( f*v.x , f*v.y);
+	// 	}
+	// 
+	// 
+	// public function convertToNormal():void
+	// {
+	// 	var tempx:Number = x; 
+	// 	x = -y; 
+	// 	y = tempx; 
+	// 	
+	// 	
+	// }		
+	// public function getNormal():Vector2
+	// {
+	// 	
+	// 	return new Vector2(-y,x); 
+	// 	
+	// }
+	// 
+	// 
+	// 
+	// public function getClosestPointOnLine ( vectorposition : Point, targetpoint : Point ) : Point
+	// {
+	// 	var m1 : Number = y / x ;
+	// 	var m2 : Number = x / -y ;
+	// 	
+	// 	var b1 : Number = vectorposition.y - ( m1 * vectorposition.x ) ;
+	// 	var b2 : Number = targetpoint.y - ( m2 * targetpoint.x ) ;
+	// 	
+	// 	var cx : Number = ( b2 - b1 ) / ( m1 - m2 ) ;
+	// 	var cy : Number = m1 * cx + b1 ;
+	// 	
+	// 	return new Point ( cx, cy ) ;
+	// }
+	// 
+
+};
+Vector2Const = {
+	TO_DEGREES : 180 / Math.PI,		
+	TO_RADIANS : Math.PI / 180,
+	temp : new Vector2()
+	};
+var rng = function(){};
+rng.range=(a,b) =>{
+    return Math.floor(Math.random() * (b-a) + a)
 }
+// rng.range=(a) =>{
+//     return Math.floor(Math.random() * a)
+// }
+rng.item=(s) =>{
+    var index = rng.range(0, s.length)
+    // console.log(s[index]);
+    return s[index];
+}
+rng.oneIn=(n) =>{
+    return rng.range(n)==0
+}
+// class Vector2D{
+//     constructor(x,y){
+//         this.x=x
+//         this.y=y
+//     }
+// }
 
 /**
  * 判断两个房间是否重叠
  **/
 function IsOverlap(room, other) {
-    xx = Math.max(room.x + room.width, other.x + other.width) - Math.min(room.x, other.x)
-    yy = Math.max(room.y + room.height, other.y + other.height) - Math.min(room.y, other.y)
+    xx = Math.max(room.getRight(), other.getRight()) - Math.min(room.getLeft(), other.getLeft())
+    yy = Math.max(room.getBottom(), other.getBottom()) - Math.min(room.getTop(), other.getTop())
     if (xx <= room.width + other.width && yy <= room.height + other.height) {
         return true
     }
@@ -38,35 +328,36 @@ function IsOverlap(room, other) {
  */
 function addRooms(n) {
     for (let i = 0; i < n; i++) {
-        size = Math.floor(Math.random() * (2 + roomExtraSize) + 1) * 2 + 1;
-        rectangularity = Math.floor(Math.random() * (1 + size / 2)) * 2;
+        size = rng.range(1, 3 + roomExtraSize) * 2 + 1;
+        rectangularity = rng.range(0, 1 + size / 2)* 2;
         // console.log(size,rectangularity);
         w = size;
         h = size;
-        if (Math.random() < 0.5) {
+        if (rng.oneIn(2)) {
             w += rectangularity;
         } else {
             h += rectangularity
         }
-        x=Math.floor(Math.random()*(WIDTH/SIZE-w-1)/2)*2+1
-        y=Math.floor(Math.random()*(HEIGHT/SIZE-h-1)/2)*2+1
-        room = new Room(x,y,w,h);
+        x = rng.range(0, (WIDTH / SIZE - w - 1) / 2) * 2 + 1
+        y = rng.range(0, (HEIGHT / SIZE - h - 1) / 2) * 2 + 1
+        room = new Rect(x,y,w,h);
         // console.log(x,y,w,h);
         overlaps = false;
-        for(let j=0;j<Rooms.length;j++)
-            {
-                if (IsOverlap(room, Rooms[j])) {
-                    overlaps = true;
-                    break;
-                }
+        for(let other in _rooms) {
+            // console.log(_rooms,other);
+            if (IsOverlap(room, _rooms[other])) {
+                overlaps = true;
+                break;
             }
+        }
         if (overlaps) continue;
-        Rooms.push(room);
+        _rooms.push(room);
         startRegion();
-        for(let i=x;i<x+w;i++){
-            for(let j=y;j<y+h;j++){
-                carve([i,j]);
-            }
+        // console.log(new Rect(x,y,w,h).points());
+        temp = new Rect(x,y,w,h).points()
+        for(var pos in temp){ 
+        // console.log(temp[pos]);
+            carve(temp[pos]);
         }
     }
     // console.log("生成房间数：", currentRegion);
@@ -117,9 +408,10 @@ function Carve(x,y){
     ctx.fillRect(x*SIZE, y*SIZE, SIZE, SIZE)
 }
 function carve(pos) {
-    ctx.fillRect(pos[0]*SIZE, pos[1]*SIZE, SIZE , SIZE)
-    MAP[pos[0]][pos[1]] = 1;
-    _regions[pos[0]][pos[1]] = currentRegion;
+    // console.log(pos);
+    ctx.fillRect(pos.x*SIZE, pos.y*SIZE, SIZE , SIZE)
+    MAP[pos.x][pos.y] = 1;
+    _regions[pos] = _currentRegion;
   }
 function CanCarve(cell,dir){
     // 必须在边界内结束。
@@ -186,58 +478,81 @@ function growMaze(start){
     }
 }
 
+function _addJunction(connector){
+    MAP[connector.x][connector.y] = 1
+    ctx.fillRect(connector.x*SIZE,connector.y*SIZE, SIZE , SIZE)
+}
+
 function connectRegions() {
-    var connectorRegions=Array.from(Array(WIDTH/SIZE), () => new Array(HEIGHT/SIZE));     // TODO
-    connectors = new Array()
-    for(let x=1;x<WIDTH/SIZE-1;x++){
-        for(let y=1;y<HEIGHT/SIZE-1;y++){
-            if(getTile([x,y])!=0) continue;
-            var regions_ = new Set();
-            POSSIBLE_DIR.forEach(dir=>{
-                // console.log(x+dir[0],y+dir[1]);
-                // console.log(_regions);
-                var region = _regions[x+dir[0]][y+dir[1]];   //TODO????
-                if(region!=null) regions_.add(region);
-            });
+    var connectorRegions=new Set();     // TODO
+    var connectors = new Array()
+    let temp = new Rect(x,y,w,h).inflate(-1).points();
+    for(var i in temp){ 
+        var pos = temp[i];
+        if(getTile(pos) != 0) continue;
+        var regions = new Set();
+        POSSIBLE_DIR.forEach(dir=>{
+            // let aaaa=pos.add(dir)
+            // console.log(pos,dir,aaaa);
+            var region = _regions[pos.add(dir)];   //TODO????
+            console.log(region);
+
+            if(region != null) regions.add(region);
+        });
+        if(regions.size<2) continue;
+        connectorRegions[pos] = regions;
+        console.log('connectorRegions:',connectorRegions);
+        connectors.push(pos)
+    }
+    console.log(regions);
+    // for(let x=1;x<WIDTH/SIZE-1;x++){
+    //     for(let y=1;y<HEIGHT/SIZE-1;y++){
+    //         if(getTile([x,y])!=0) continue;
+    //         var regions_ = new Set();
+    //         POSSIBLE_DIR.forEach(dir=>{
+    //             // console.log(x+dir[0],y+dir[1]);
+    //             // console.log(_regions);
+    //             var region = _regions[x+dir[0]][y+dir[1]];   //TODO????
+    //             if(region!=null) regions_.add(region);
+    //         });
         
 
-            if(regions_.length<2) continue;
-            connectorRegions[x][y] = regions_;
-            connectors.push([x,y])
-        }
-    }
+    //         if(regions_.length<2) continue;
+    //         connectorRegions[x][y] = regions_;
+    //         connectors.push([x,y])
+    //     }
+    // }
 
     // console.log(connectors);
 
-    var merged = [];
+    var merged = {};
     openRegions = new Set();
-    for (var i = 0; i <= currentRegion; i++) {
+    for (var i = 0; i <= _currentRegion; i++) {
         merged[i] = i;
         openRegions.add(i);
     }
 
     while (openRegions.size > 1) {
-        let index = Math.floor(Math.random()*connectors.length)
-        // console.log(connectors[index]);
-        var connector = connectors[index];
+        // let index = Math.floor(Math.random()*connectors.length)
+        var connector = rng.item(connectors);
+        console.log(connectors);
   
         // TODO
         // _addJunction(connector);
         // console.log(connector);
-        MAP[connector[0]][connector[1]] = 1
-        ctx.fillRect(connector[0]*SIZE,connector[1]*SIZE, SIZE , SIZE)
   
         // Merge the connected regions. We'll pick one region (arbitrarily) and
         // map all of the other regions to its index.
-        // console.log(Array.from(connectorRegions[connector[0]][connector[1]]));
-        var regions = Array.from(connectorRegions[connector[0]][connector[1]]).map((region) => merged[region]);
-        var dest = regions[regions.length-1];
-        var sources = regions.slice(2);
+        // console.log(connectorRegions[connector]);
+        // if(connectorRegions[pos]==null) continue;
+        var regions = new Set(Array.from(connectorRegions[connector]).map((region) => merged[region]));
+        var dest = regions[0];
+        var sources = Array.from(regions).slice(2);
   
         // Merge all of the affected regions. We have to look at *all* of the
         // regions because other regions may have previously been merged with
         // some of the ones we're merging now.
-        for (var i = 0; i <= currentRegion; i++) {
+        for (var i = 0; i <= _currentRegion; i++) {
           if (sources.includes(merged[i])) {
             merged[i] = dest;
           }
@@ -249,12 +564,17 @@ function connectRegions() {
         })
         temp=[]
         for(let i=0;i<connectors.length;i++) {
-            if (connector - pos < 2) continue
-            var regions = new Set(Array.from(connectorRegions[pos[0]][pos[1]]).map((region) => merged[region]));
-              if (regions.size > 1) temp.push(pos);
+            console.log(connector,pos);
+            if (connector.isCloseTo(pos, 2)) continue;
+            // if (connector - pos < 2) continue;
+            console.log(connectorRegions,pos,connectorRegions[pos]);
+            if(connectorRegions[pos]==null) continue;
+            var regions = new Set(Array.from(connectorRegions[pos]).map((region) => merged[region]));
+            if (regions.size > 1) temp.push(pos);
+            if(rng.oneIn(extraConnectorChance)) _addJunction(pos);
         }
         connectors=temp;
-        console.log(connectors.length);
+        // console.log(connectors.length);
         // Remove any connectors that aren't needed anymore.
         // connectors.removeWhere((pos) {
         //   // Don't allow connectors right next to each other.
@@ -276,24 +596,20 @@ function connectRegions() {
 }
 
 function startRegion() {
-    currentRegion++;
+    _currentRegion++;
 }
 
 function getTile(pos) {
-    return MAP[pos[0]][pos[1]]
-}
-
-function _addJunction(){
-
+    return MAP[pos.x][pos.y]
 }
 
   //房间额外大小
 roomExtraSize = 0
 windingPercent = 0
-Rooms = []
+_rooms = []
 ind = 0
   /// 当前区域的索引正在雕刻。
-currentRegion = -1;
+_currentRegion = -1;
 ctx.fillStyle = COLOR[ind];
 // ctx.fillRect(0,0,SIZE,SIZE)
 /*
@@ -306,14 +622,17 @@ for (i = 0;i< WIDTH / SIZE;i++) {
 */
 MAP = new Array(WIDTH/SIZE).fill(0).map(v => new Array(HEIGHT/SIZE).fill(0));
 // console.log(MAP);
-_regions = new Array(WIDTH/SIZE).fill(0).map(v => new Array(HEIGHT/SIZE).fill(0));
+// _regions = new Array(WIDTH/SIZE).fill(0).map(v => new Array(HEIGHT/SIZE).fill(0));
+_regions = new Array();
 // console.log(_regions);
 console.log('添加房间');
 addRooms(20)
+console.log('生长树算法');
+
 ctx.fillStyle = "purple"
 for (let y = 1; y < HEIGHT/SIZE-1; y += 2) {
     for (let x = 1; x < WIDTH/SIZE-1; x += 2) {
-        let pos = [x,y];
+        let pos = new Vector2(x,y);
         if (getTile(pos) != 0) continue;
         growMaze(pos);
     }
